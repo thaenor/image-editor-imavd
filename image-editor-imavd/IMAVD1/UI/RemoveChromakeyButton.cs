45a6c50db7ea3d8705f3821907f5f7ef2bb61021
krakenyzer <mailforspam20@gmail.com>
2019-05-05 00:28:41 +0100

    fix: chromakey

--- image-editor-imavd/IMAVD1/UI/RemoveChromakeyButton.cs
+++ image-editor-imavd/IMAVD1/UI/RemoveChromakeyButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace IMAVD1.UI
@@ -21,7 +22,40 @@ namespace IMAVD1.UI
         {
             form.Cursor = Cursors.Cross;
             this.form.activateChromakeyClick();
+            this.form.imgPicBox.MouseClick += new MouseEventHandler(imageMouseClick);
+        }
 
+        private void imageMouseClick(object sender, MouseEventArgs e)
+        {
+            form.Cursor = Cursors.Cross;
+
+            Bitmap img = new Bitmap(this.form.imgPicBox.Image);
+
+            Color color = img.GetPixel(e.X, e.Y);
+            Image image = form.imgPicBox.Image;
+            int width = image.Width;
+            int height = image.Height;
+
+            Bitmap imageBitMap = new Bitmap(image);
+            int counter = 0;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color pixelColor = imageBitMap.GetPixel(i, j);
+                    if (color.ToArgb().Equals(pixelColor.ToArgb()))
+                    {
+                        Color newColor = img.GetPixel(i, j);
+                        newColor = Color.FromArgb(0, 255, 255, 255);
+                        img.SetPixel(i, j, newColor);
+                    }
+                }
+            }
+            form.NewImageStack(img);
+
+            this.form.imgPicBox.MouseClick -= new MouseEventHandler(imageMouseClick);
+            this.form.Cursor = Cursors.Default;
         }
     }
 }
